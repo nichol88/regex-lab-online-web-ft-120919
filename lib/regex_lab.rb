1e7644f90a96c3376f5af2087f419e91c7ff961d
Brian Nicholls <nichol88@gmail.com>
2020-01-25 21:16:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,5 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone =~ /[0-9]{3}\D[0-9]{3}\D[0-9]{4}/
+  !!(phone =~ /[0-9]{3}\D[0-9]{3}\D[0-9]{4}/)
 end
