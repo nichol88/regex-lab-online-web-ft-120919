f68de0f81ce6bd44e2a0141598d3d72c78451d2d
Brian Nicholls <nichol88@gmail.com>
2020-01-25 20:36:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,6 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  m = word.match(/\b[aeiouAEIOU]\b|\S+/)
-  binding.pry
-  m
+  /\b[aeiouAEIOU]\S+/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
